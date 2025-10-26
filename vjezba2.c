7087a4cb52534cb4088004b2892442ce9d1fdbda
IvanVujevic <ivanvujevic720@gmail.com>
2025-10-26 14:04:27 +0100

    Update vjezba2.c

--- vjezba2.c
+++ vjezba2.c
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define SCANF_ERROR -2
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<malloc.h>
 #define MAX 50
 
 typedef struct _person* position;
@@ -11,58 +11,80 @@ typedef struct _person {
 	char surname[MAX];
 	int birthyear;
 	position next;
-	}person;
+}person;
 
-position createPerson(char *, char *, int);
+position createPerson(char*, char*, int);
 int addToFront(position, char* name, char* surname, int birthyear);
-int printlist(position);
+int printList(position);
 int addToEnd(position, char* name, char* surname, int birthyear);
 int searchBySurname(position, char*);
 int removePerson(position, char*);
 int removeList(position);
-int findPrev(position, char*);
+position findPrev(position, char*);
 
 int main() {
-	person head = { .name = {0}, .surname = {0}, .birthyear=0, .next=NULL };
-	char ch=0;
-	char inputName=0;
-	char inputSurname=0;
-	char inputBirthyear=0;
+	person head = { .name = {0}, .surname = {0}, .birthyear = 0, .next = NULL };
+	char ch = 0;
+	char inputName[MAX] = {0};
+	char inputSurname[MAX] = {0};
+	int inputBirthyear = 0;
 
 	addToFront(&head, "Ivan", "Ivic", 2000);
 	addToEnd(&head, "Ana", "Anic", 2001);
-	printf("Which of the following actions do you want to execute:\na - add to the front of the list\nb - print out the list\nc - add to the end of the list\nd - search by surname in the list\ne - delete element from the list\n}");
-	scanf("%s", &ch);
-	
-	switch(ch){
+	printf("a - add to the front of the list\nb - print out the list\nc - add to the end of the list\nd - search by surname in the list\ne - delete element from the list\n0 - end program\n");
+
+	do {
+		printf("Which of the following actions do you want to execute: ");
+		if (scanf(" %c", &ch) != 1) return SCANF_ERROR;
+
+		switch (ch) {
 
 		case 'a':
 			printf("add to the front of the list:\nInsert name, surname and birthyear: ");
 			scanf("%s %s %d", &inputName, &inputSurname, &inputBirthyear);
-			addToFront(&head, &inputName, &inputSurname, &inputBirthyear);
+			addToFront(&head, inputName, inputSurname, inputBirthyear);
 			break;
-		
+
 		case 'b':
 			printf("\nprint out the list\n");
-			printlist(&head);
+			printList(&head);
 			break;
-		
+
 		case 'c':
 			printf("add to the end of the list:\nInsert name, surname and birthyear: ");
 			scanf("%s %s %d", &inputName, &inputSurname, &inputBirthyear);
-			addToEnd(&head, &inputName, &inputSurname, &inputBirthyear);
+			addToEnd(&head, inputName, inputSurname, inputBirthyear);
 			break;
-	}
+
+		case 'd':
+			printf("search by surname in the list");
+			scanf("%s", &inputSurname);
+			searchBySurname(&head, inputSurname);
+			break;
+
+		case 'e':
+			printf("delete element from the list");
+			scanf("%s", &inputSurname);
+			removePerson(&head, inputSurname);
+			break;
+
+		case '0':
+			break;
+
+		default:
+			printf("Invalid input");
+			break;
+		}
+	} while (ch != '0');
 
 
-	//8ipo navecer subota ako ovo citate, mislin se zadnje 3 ure kako ovaj switch uvalit da radi sa skeniranjen imena (na petak san ima previse obaveza)
 	removeList(&head);
 	return 1;
 }
 
-position createPerson(char* name, char * surname, int birthyear) {
-	position newPerson=(position)malloc(sizeof(person));
-	if (newPerson = NULL) {
+position createPerson(char* name, char* surname, int birthyear) {
+	position newPerson = (position)malloc(sizeof(person));
+	if (newPerson == NULL) {
 		printf("Malloc error!");
 		return NULL;
 	}
@@ -82,14 +104,14 @@ int addToFront(position head, char* name, char* surname, int birthyear) {
 
 int printList(position head) {
 	position start = head->next;
-	while (start->next != NULL) {
-		printf("%s\t%s\t%d\n",start->name, start->surname, start->birthyear);
+	while (start != NULL) {
+		printf("\n%s\t%s\t%d\n", start->name, start->surname, start->birthyear);
 		start = start->next;
 	}
 	return EXIT_SUCCESS;
 }
 
-int addToEnd(position head, char* name, char* surname, int birthyear){
+int addToEnd(position head, char* name, char* surname, int birthyear) {
 	position p = createPerson(name, surname, birthyear);
 	while (head->next != NULL) {
 		head = head->next;
@@ -99,9 +121,9 @@ int addToEnd(position head, char* name, char* surname, int birthyear){
 	return EXIT_SUCCESS;
 }
 
-int searchBySurname(position head, char * surname) {
+int searchBySurname(position head, char* surname) {
 	position start = head->next;
-	while (start->next != NULL) {
+	while (start != NULL) {
 		if (strcmp(start->surname, surname) == 0) {
 			printf("%s\t%s\t%d\n", start->name, start->surname, start->birthyear);
 		}
@@ -124,8 +146,8 @@ int removePerson(position head, char* surname) {
 	return EXIT_SUCCESS;
 }
 
-int findPrev(position current, char * surname) {
-	while (current->next != NULL && strcmp(current->next->surname, surname)==0)
+position findPrev(position current, char* surname) {
+	while (current->next != NULL && strcmp(current->next->surname, surname) != 0)
 		current = current->next;
 
 	if (current->next == NULL) return NULL;
@@ -145,5 +167,4 @@ int removeList(position head) {
 	return EXIT_SUCCESS;
 
 }
-
 
